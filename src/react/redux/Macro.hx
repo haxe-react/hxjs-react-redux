772ef87502d8ac6b70ffd3a8660b23fca2052cc4
kevinresol <kevinresol@gmail.com>
2016-11-22 10:38:30 +0800

    Update connect macro

--- src/react/redux/Macro.hx
+++ src/react/redux/Macro.hx
@@ -8,12 +8,14 @@ class Macro {
 		var pos = Context.currentPos();
 		var cls = Context.getLocalClass().get();
 		var fields = Context.getBuildFields();
+		var name = cls.module.split('.');
+		if(name[name.length - 1] != cls.name) name.push(cls.name);
 		fields.push({
 			access: [AStatic],
 			name: '__init__',
 			kind: FFun({
 				args: [],
-				expr: macro @:pos(pos) untyped $i{cls.name} = react.ReactRedux.connect(mapStateToProps, mapDispatchToProps)($e),
+				expr: macro @:pos(pos) untyped __js__('{0} = {1}', $p{name}, react.ReactRedux.connect(mapStateToProps, mapDispatchToProps)($e)),
 				ret: null,
 			}),
 			pos: pos,
